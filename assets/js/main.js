b55d7ff1c4349e9afc6836b031e3fa624c7f3ccd
stephenseither <stephen.seither@gmail.com>
2020-01-20 21:52:31 +0000

    rework

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,16 +20,6 @@ class MemoryGame {
         this.shuffle();
     }
 
-    startCountDown () {
-        return setInterval(() => {
-            this.timeRemaining--;
-            this.timer.innerText = this.timeRemaining;
-            if (this.timeRemaining === 0) {
-                this.gameOver();
-            }
-        }, 1000)
-    }
-
     shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -53,4 +43,80 @@ function ready () {
         });
     });
     
+}
+
+$("#play").click( function(){
+   var counter = 120;
+   setInterval(function() {
+     counter--;
+      if (counter >= 0) {
+         span = document.getElementById("timer");
+         span.innerHTML = counter;
+      }
+      if (counter === 0) {
+         alert('Game Over');
+         clearInterval(counter);
+       }
+     }, 1000);
+});
+
+
+
+
+
+class MemoryGame {
+    constructor (pics) {
+        this.picArray = pics;
+    }
+
+    startGame() {
+        this.totalClicks = 0;
+        this.picsToCheck = null;
+        this.matchedPics = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.shuffle(this.picsArray);
+            this.busy = false;
+        }, 500)
+        this.hidePics();
+        this.ticker.innerText = this.totalClicks;
+    }
+
+    gameOver () {
+        clearInterval(handler);
+        alert("Game Over!")
+    }
+
+    victory() {
+        clearInterval(handler);
+        alert("Congratulations! You have won!");
+    }
+
+    hidePics() {
+        this.picArray.forEach(pic => {
+            pic.classList.remove('visisble');
+            pic.classList.remove('matched');
+        });
+    }
+
+    flipPic(pic) {
+        if(this.canFlip(pic)) {
+            this.totalClicks ++;
+            this.ticker.innerText = this.totalClicks;
+            pic.classList.add('visible');
+
+            if(this.picToCheck) {
+                this.checkforPicMatch(pic);
+            } else {
+                this.picToCheck = pic;
+            }
+        }
+    }
+    shuffle(cardsArray) {
+        for (let i = picArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i + 1));
+            picArray[randIndex].style.order = i;
+            picArray[i].style.order = randIndex;
+        }
+    }
 }
